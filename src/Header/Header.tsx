bf113f1c891b1fc820a19560d90fcf0fc29222c6
ayoubqrt <moustakim.ayoub@hotmail.com>
2022-05-31 14:31:21 +0200

    chore: added new favicons and modify manifest

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,20 +1,20 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 import Typography from '@material-ui/core/Typography';
-import { AppBar, Toolbar } from "@material-ui/core";
+import { AppBar, Toolbar } from '@material-ui/core';
 
 import logo from './logo.png';
 
 const Header = () => {
   return (
-    <AppBar style={{height: "64px"}} position="static">
+    <AppBar style={{ height: '64px' }} position="static">
       <Toolbar>
         <Link to="/">
-          <Typography variant="h6" style={{color: "white"}}>
+          <Typography variant="h6" style={{ color: 'white' }}>
             La Berri BlindTest
           </Typography>
         </Link>
-        <img src={logo} style={{height: "40px", marginLeft:"10px"}}/>
+        <img src={logo} style={{ height: '40px', marginLeft: '10px' }} />
       </Toolbar>
     </AppBar>
   );
